ef15f4ab266904569bcc4bfd8ec45bd747d86ad7
lh <1072344372@qq.com>
2023-09-16 18:36:19 +0800

    实现阶段4-开发Mapper接口和Mapper.xml

---
+++ src/main/java/asia/lhweb/mapper/MonsterMapper.java
@@ -0,0 +1,13 @@
+package asia.lhweb.mapper;
+
+import asia.lhweb.entity.Monster;
+
+/**
+ * @author :罗汉
+ * @date : 2023/9/16
+ */
+public interface MonsterMapper {
+
+    //查询方法
+    Monster getMonsterById(Integer id);
+}
